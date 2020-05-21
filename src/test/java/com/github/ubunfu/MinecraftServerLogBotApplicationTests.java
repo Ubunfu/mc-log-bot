071f2560219e56b1f373bdbd01d0874b1c15606f
Ryan Allen <rallen3882@gmail.com>
2020-05-21 02:13:47 -0400

    [RAllen] Init commit

---
+++ src/test/java/com/github/ubunfu/MinecraftServerLogBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ubunfu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinecraftServerLogBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
