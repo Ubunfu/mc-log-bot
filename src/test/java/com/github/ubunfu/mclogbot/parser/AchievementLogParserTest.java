c782aaf63651d62c97a7e1966c8fce6a7fdc33f7
Ryan Allen <rallen3882@gmail.com>
2020-06-07 16:40:24 -0400

    [RAllen] Fixing typo in LogParser test

--- src/test/java/com/github/ubunfu/mclogbot/parser/AchievementLogParserTest.java
+++ src/test/java/com/github/ubunfu/mclogbot/parser/AchievementLogParserTest.java
@@ -11,8 +11,8 @@ import static org.hamcrest.Matchers.equalTo;
 public class AchievementLogParserTest {
 
     private static final String PLAYER = "PLAYER_001";
-    private static final String ACHIEVMENT = "Who's the Pillager Now?";
-    private static final String LOG_VALID = "[01:27:02] [Server thread/INFO]: " + PLAYER + " has made the advancement [" + ACHIEVMENT + "]";
+    private static final String ACHIEVEMENT = "Who's the Pillager Now?";
+    private static final String LOG_VALID = "[01:27:02] [Server thread/INFO]: " + PLAYER + " has made the advancement [" + ACHIEVEMENT + "]";
     private Field fieldPlayer;
     private Field fieldAchievement;
 
@@ -22,7 +22,7 @@ public class AchievementLogParserTest {
     void setUp() {
         logParser = new AchievementLogParser();
         fieldPlayer = new Field("Player", PLAYER);
-        fieldAchievement = new Field("Achievement", ACHIEVMENT);
+        fieldAchievement = new Field("Achievement", ACHIEVEMENT);
     }
 
     @Test
