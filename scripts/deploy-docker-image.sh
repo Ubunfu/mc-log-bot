6053d7ef72410a58656eceb1f703effb7ade1988
Ryan Allen <rallen3882@gmail.com>
2021-05-03 20:55:56 -0400

    pointing build/deploy scripts to GHCR

--- scripts/deploy-docker-image.sh
+++ scripts/deploy-docker-image.sh
@@ -2,4 +2,4 @@
 
 VERSION=$1
 
-docker buildx build --platform linux/amd64,linux/arm64 -t ubunfu/mc-log-bot:latest -t ubunfu/mc-log-bot:$VERSION --push ../
+docker buildx build --platform linux/amd64,linux/arm64 -t ghcr.io/ubunfu/mc-log-bot/mc-log-bot:$VERSION -t ubunfu/mc-log-bot:$VERSION --push ../
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,9 @@
 # brew install jq python-yq
 VERSION=$(xq .project.version ../pom.xml | tr -d \")
 
+echo ""
+echo "Deploying tag ${VERSION}"
+echo ""
+
 ./deploy-github-tag.sh $VERSION
 ./deploy-docker-image.sh $VERSION
